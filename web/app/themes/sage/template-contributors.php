88dd7ea13f4dea630257d536b6de0d75489fa9f6
Akshat Kedia <akshat.kedia@outlook.com>
2015-12-10 20:33:55 +0530

    Added COntributors Listing Template and reversed the show sidebar function.

---
+++ web/app/themes/sage/template-contributors.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Template Name: Contributors Listing Template
+ */
+?>
+
+<?php get_template_part('templates/content', 'contributors'); ?>
---
+++ web/app/themes/sage/templates/content-contributors.php
@@ -0,0 +1,54 @@
+<div>
+  <div>
+    <div class="col-sm-12">
+      <h2>Editor</h2>
+    </div>
+    <div>
+      <?php $jugaad_editor = get_users('role=editor&orderby=post_count&order=DESC'); ?>
+      <?php foreach($jugaad_editor as $user): ?>
+      <div class="col-sm-4">
+        <article <?php post_class(); ?>>
+          <h3><a href='<?php echo get_author_posts_url($user->ID); ?>'><?php echo esc_html($user->display_name); ?></a></h3>
+          <div><?php echo get_the_author_meta('description', $user->ID); ?></div>
+        </article>
+      </div>
+      <?php endforeach; ?>
+    </div>
+  </div>
+  <div>
+    <div class="col-sm-12">
+      <h2>Authors</h2>
+    </div>
+    <div>
+      <?php $jugaad_authors = get_users('role=author&orderby=post_count&order=DESC&exclude=13'); ?>
+      <?php foreach($jugaad_authors as $user): ?>
+      <?php if(get_the_author_meta('description', $user->ID) != ''): ?>
+      <div class="col-sm-4">
+        <article <?php post_class(); ?>>
+          <h3><a href='<?php echo get_author_posts_url($user->ID); ?>'><?php echo esc_html($user->display_name); ?></a></h3>
+          <div><?php echo get_the_author_meta('description', $user->ID); ?></div>
+        </article>
+      </div>
+      <?php endif; ?>
+      <?php endforeach; ?>
+    </div>
+  </div>
+  <div>
+    <div class="col-sm-12">
+      <h2>Contributors</h2>
+    </div>
+    <div>
+      <?php $jugaad_contributors = get_users('role=contributor&orderby=post_count&order=DESC'); ?>
+      <?php foreach($jugaad_contributors as $user): ?>
+      <?php if(get_the_author_meta('description', $user->ID) != ''): ?>
+      <div class="col-sm-4">
+        <article <?php post_class(); ?>>
+          <h3><a href='<?php echo get_author_posts_url($user->ID); ?>'><?php echo esc_html($user->display_name); ?></a></h3>
+          <div><?php echo get_the_author_meta('description', $user->ID); ?></div>
+        </article>
+      </div>
+      <?php endif; ?>
+      <?php endforeach; ?>
+    </div>
+  </div>
+</div>
